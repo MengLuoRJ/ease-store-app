f42709b99ffcd3d7d4cde4af05c1e2523fa03963
MengLuoRJ <mengluor@gmail.com>
2023-07-04 02:46:19 +0800

    feat: add Vant emulator and lazyload

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
+import '@vant/touch-emulator';
 import 'virtual:uno.css'
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
+import { Lazyload } from 'vant'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(Lazyload, {
+  lazyComponent: true
+})
+
+const pinia = createPinia()
+pinia.use(
+  createPersistedState({
+    storage: localStorage
+  })
+)
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
